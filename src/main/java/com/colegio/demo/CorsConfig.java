564e87e456f089a7eb93ec26c3826b87c540aa39
luis <luistran22@gmail.com>
2025-03-14 21:45:15 -0500

    commit agregando permisos a azure

--- src/main/java/com/colegio/demo/CorsConfig.java
+++ src/main/java/com/colegio/demo/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowCredentials(true);
-		configuration.setAllowedOrigins(Arrays.asList("https://registro-colegio-angular.vercel.app"));
+		configuration.setAllowedOrigins(Arrays.asList("https://registro-colegio-angular.vercel.app,https://registro-colegio.azurewebsites.net"));
 		configuration.setAllowedMethods(Arrays.asList("*"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
