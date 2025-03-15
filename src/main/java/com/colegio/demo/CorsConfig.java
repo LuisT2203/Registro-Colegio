a6ff8e739bc84f95e55e3fe508a01847fbe2032f
luis <luistran22@gmail.com>
2025-03-14 21:51:04 -0500

    commit agregando origen azure

--- src/main/java/com/colegio/demo/CorsConfig.java
+++ src/main/java/com/colegio/demo/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowCredentials(true);
-		configuration.setAllowedOrigins(Arrays.asList("https://registro-colegio-angular.vercel.app,https://registro-colegio.azurewebsites.net"));
+		configuration.setAllowedOrigins(Arrays.asList("https://registro-colegio-angular.vercel.app,https://registro-colegio-angular-azure.vercel.app"));
 		configuration.setAllowedMethods(Arrays.asList("*"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
